5cbd872d72f8d27033e8be7fdb1618ad10d9f15a
Derek <dmanhastings97@gmail.com>
2015-03-03 18:19:54 -0500

    Fixed some small things, but I am not sure if that is everything

--- src/org/first/team4533/robot/Robot.java
+++ src/org/first/team4533/robot/Robot.java
@@ -13,11 +13,7 @@ import org.first.team4533.robot.subsystems.GripperSystem;
 import org.first.team4533.robot.subsystems.LiftSystem;
 
 /**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
+ * Main class that is executed when robot is turned on
  */
 public class Robot extends IterativeRobot {
 
@@ -25,10 +21,6 @@ public class Robot extends IterativeRobot {
 
     private CameraServer cameraServer;
 
-    /**
-     * This function is run when the robot is first started up and should be
-     * used for any initialization code.
-     */
     public void robotInit() {
         cameraServer = CameraServer.getInstance();
         cameraServer.setQuality(50);
@@ -49,9 +41,6 @@ public class Robot extends IterativeRobot {
         this.autonomousCommand.start();
     }
 
-    /**
-     * This function is called periodically during autonomous
-     */
     public void autonomousPeriodic() {
         Scheduler.getInstance().run();
     }
@@ -60,24 +49,14 @@ public class Robot extends IterativeRobot {
         if (autonomousCommand != null) autonomousCommand.cancel();
     }
 
-    /**
-     * This function is called when the disabled button is hit.
-     * You can use it to reset subsystems before shutting down.
-     */
     public void disabledInit(){
 
     }
 
-    /**
-     * This function is called periodically during operator control
-     */
     public void teleopPeriodic() {
         Scheduler.getInstance().run();
     }
     
-    /**
-     * This function is called periodically during test mode
-     */
     public void testPeriodic() {
         LiveWindow.run();
     }
